16f518db22e45d5bc1d6bf11049b9d08b590160c
sambi <sambi@DESKTOP-NP6JPN5>
2017-08-27 02:26:17 -0500

    complex types with classes demo

--- src/com/example/calcengine/Main.java
+++ src/com/example/calcengine/Main.java
@@ -5,87 +5,26 @@ public class Main {
 	
 	public static void main (String args[])
 	{
-		double[] leftVals = {10, 20,30,40};
-		double[] rightVals = {50,60,70,80};
-		char[] opCodes = {'a','s','m','d'};
-		double[] results = new double[opCodes.length];
-		
 		MathEquation[] equations = new MathEquation[4];
-		equations[0] = create(10,50,'a');
+		equations[0] = create(20,50,'a');
 		equations[1] = create(20,60,'s');
 		equations[2] = create(30,70,'m');
 		equations[3] = create(40,80,'d');
 		
-		
-		
-		/* for (int i=0; i<opCodes.length; i++){
+		for (MathEquation equation :equations){
 			
-			switch(opCodes[i]){
-			
-			case 'a' :
-				results[i]=leftVals[i]+rightVals[i];
-				break;
-			case 's' :
-				results[i]=leftVals[i]-rightVals[i];
-				break;
-			case 'm' :
-				results[i]=leftVals[i]*rightVals[i];
-				break;
-			case 'd' :
-				results[i] = rightVals[i] !=0 ? leftVals[i]/rightVals[i] :0 ;
-				break;
-			default: 
-				System.out.println("Invalid opCOde");
-				results[i]=0;
-			}*/
-				
+			equation.execute();
+			System.out.println("Result is" + equation.getResult());
 		}
 		
-		
-		
-		/*for (double result :results){
-			
-			System.out.println("Result is" +result);
-		}*/
-		
-/*double val1 =100;
-double val2 = 50;
-double result=0 ;
-
-char opCode='d';
-float[] floatArr = new float[3];
-
-for (int i=0; i<=2;i++){
-	floatArr[i]=i;
-}
-
-for (float f : floatArr){
-	
-	System.out.println("Arr["+ f +" ] " + f );
-}*/
-
-/*if (opCode=='a')
-	result=val1+val2;
-else if (opCode=='s')
-	result=val1-val2;
-else if (opCode=='m')
-	result=val1*val2;
-else if (opCode=='d')
-	result= val2 >0 ? val1/val2 : 0 ;
-else{
-	System.out.println("Invalid opCode");
-	result = 0 ;
-}*/
-
-
 }
 	
 	public static MathEquation create (double leftVal , double rightVal, char opCode ){
 		
 		MathEquation equation = new MathEquation () ;
-		equation.leftVal=leftVal;
-		equation.rightVal = rightVal;
-		equation.opCode=opCode;
+		equation.setLeftVal(leftVal);
+		equation.setRightVal(rightVal);
+		equation.setOpCode(opCode);
 		
 		return equation;
 	}
--- src/com/example/calcengine/MathEquation.java
+++ src/com/example/calcengine/MathEquation.java
@@ -2,30 +2,62 @@ package com.example.calcengine;
 
 public class MathEquation {
 
-	public double leftVal ;
-	public double rightVal;
-	public char opCode;
-	public double result ;
+	private double leftVal;
+	private double rightVal;
+	private char opCode;
+	private double result;
 	
-	public void execute (){
-		
-		switch(opCode){
-		
-		case 'a' :
-			result=leftVal+rightVal;
+	public double getRightVal() {
+		return rightVal;
+	}
+
+	public void setRightVal(double rightVal) {
+		this.rightVal = rightVal;
+	}
+
+	public char getOpCode() {
+		return opCode;
+	}
+
+	public void setOpCode(char opCode) {
+		this.opCode = opCode;
+	}
+
+	public double getResult() {
+		return result;
+	}
+
+
+
+
+
+	public double getLeftVal() {
+		return leftVal;
+	}
+
+	public void setLeftVal(double leftVal) {
+		this.leftVal = leftVal;
+	}
+
+	public void execute() {
+
+		switch (opCode) {
+
+		case 'a':
+			result = leftVal + rightVal;
 			break;
-		case 's' :
-			result=leftVal-rightVal;
+		case 's':
+			result = leftVal - rightVal;
 			break;
-		case 'm' :
-			result=leftVal*rightVal;
+		case 'm':
+			result = leftVal * rightVal;
 			break;
-		case 'd' :
-			result = rightVal !=0 ? leftVal/rightVal :0 ;
+		case 'd':
+			result = rightVal != 0 ? leftVal / rightVal : 0;
 			break;
-		default: 
+		default:
 			System.out.println("Invalid opCOde");
-			result=0;
+			result = 0;
 		}
 	}
 }
